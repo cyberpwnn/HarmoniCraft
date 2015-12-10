95bd19f5381b491f22caf0b9a4a9824adbec16c8
danielmills <danielmillst@gmail.com>
2015-12-10 01:00:42 -0500

    Finish package api

--- src/main/java/com/ulticraft/hc/component/PackageComponent.java
+++ src/main/java/com/ulticraft/hc/component/PackageComponent.java
@@ -1,18 +1,21 @@
 package com.ulticraft.hc.component;
 
 import java.io.File;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.entity.Player;
 import com.ulticraft.hc.HarmoniCraft;
 import com.ulticraft.hc.composite.PackageData;
 import com.ulticraft.hc.uapi.Component;
 import com.ulticraft.hc.uapi.DataManager;
 import com.ulticraft.hc.uapi.Depend;
 import com.ulticraft.hc.uapi.UList;
+import com.ulticraft.hc.uapi.UMap;
 
 @Depend(DataComponent.class)
 public class PackageComponent extends Component
 {
 	private File base;
-	private UList<PackageData> packages;
+	private UMap<String, PackageData> packages;
 	
 	public PackageComponent(HarmoniCraft pl)
 	{
@@ -22,7 +25,7 @@ public class PackageComponent extends Component
 	public void enable()
 	{
 		this.base = new File(pl.getDataFolder(), "packages");
-		this.packages = new UList<PackageData>();
+		this.packages = new UMap<String, PackageData>();
 		
 		if(!base.exists())
 		{
@@ -49,7 +52,7 @@ public class PackageComponent extends Component
 				{
 					PackageData pd = (PackageData) dm.readYAML(PackageData.class);
 					
-					packages.add(pd);
+					packages.put(StringUtils.remove(i.getName(), ".yml").toLowerCase(), pd);
 					
 					pl.o("Loded Package: " + pd.getName() + " [" + pd.getCost() + " Notes]");
 					l++;
@@ -63,5 +66,88 @@ public class PackageComponent extends Component
 		}
 		
 		pl.s("Loaded " + l + " Packages");
+	}
+	
+	public PackageData get(String name)
+	{
+		for(String i : packages.keySet())
+		{
+			if(i.equalsIgnoreCase(name))
+			{
+				return packages.get(i);
+			}
+		}
+		
+		for(PackageData i : packages.values())
+		{
+			if(i.getName().equals(name))
+			{
+				return i;
+			}
+			
+			if(i.getName().equals(name))
+			{
+				return i;
+			}
+		}
+		
+		return null;
+	}
+	
+	public UList<PackageData> get()
+	{
+		return new UList<PackageData>(packages.values());
+	}
+	
+	public UList<PackageData> get(Player p)
+	{
+		UList<String> pd = new UList<String>(pl.gpd(p).getPackages());
+		UList<PackageData> pck = new UList<PackageData>();
+		
+		for(String i : pd)
+		{
+			PackageData pk = get(i);
+			
+			if(pk != null)
+			{
+				pck.add(pk);
+			}
+		}
+		
+		return pck;
+	}
+	
+	public String getCodeName(PackageData pd)
+	{
+		for(String i : packages.keySet())
+		{
+			if(packages.get(i).equals(pd))
+			{
+				return i;
+			}
+		}
+		
+		return null;
+	}
+	
+	public boolean has(Player p, PackageData pd)
+	{
+		return get(p).contains(pd);
+	}
+	
+	public boolean has(Player p, String pd)
+	{
+		return get(p).contains(get(pd));
+	}
+	
+	public void aquire(Player p, PackageData pd)
+	{
+		if(has(p, pd) || !pl.getNoteComponent().has(p, pd.getCost()))
+		{
+			return;
+		}
+		
+		pl.gpd(p).setPackages(new UList<String>(pl.gpd(p).getPackages()).qadd(getCodeName(pd)).toString(","));
+		pl.getNoteComponent().take(p, pd.getCost());
 	}
 }
