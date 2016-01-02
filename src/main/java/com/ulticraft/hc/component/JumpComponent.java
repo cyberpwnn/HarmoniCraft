ca98c64c88090906ae757137db674dc20df8e29b
ballyworld <christianking01@yahoo.com>
2016-01-01 18:39:16 -0800

    add JumpComponet

---
+++ src/main/java/com/ulticraft/hc/component/JumpComponent.java
@@ -0,0 +1,25 @@
+package com.ulticraft.hc.component;
+
+import com.ulticraft.hc.HarmoniCraft;
+import com.ulticraft.hc.uapi.Component;
+
+public class JumpComponent extends Component {
+
+	public JumpComponent(HarmoniCraft pl) {
+		super(pl);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void enable() {
+		// TODO Auto-generated method stub
+		super.enable();
+	}
+
+	@Override
+	public void disable() {
+		// TODO Auto-generated method stub
+		super.disable();
+	}
+
+}
