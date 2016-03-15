370ee15ef11615cf9f8b6deb954b42076092894f
danielmills <danielmillst@gmail.com>
2016-03-15 19:51:35 -0400

    Permission and command integration

--- src/main/java/com/ulticraft/hc/component/PackageComponent.java
+++ src/main/java/com/ulticraft/hc/component/PackageComponent.java
@@ -1,21 +1,28 @@
 package com.ulticraft.hc.component;
 
 import java.io.File;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import com.ulticraft.hc.HarmoniCraft;
+import com.ulticraft.hc.Info;
 import com.ulticraft.hc.composite.PackageData;
 import com.ulticraft.hc.uapi.Component;
 import com.ulticraft.hc.uapi.DataManager;
 import com.ulticraft.hc.uapi.Depend;
 import com.ulticraft.hc.uapi.UList;
 import com.ulticraft.hc.uapi.UMap;
+import net.md_5.bungee.api.ChatColor;
+import net.milkbowl.vault.permission.Permission;
 
 @Depend(DataComponent.class)
 public class PackageComponent extends Component
 {
 	private File base;
 	private UMap<String, PackageData> packages;
+	public static Permission perms = null;
 	
 	public PackageComponent(HarmoniCraft pl)
 	{
@@ -27,15 +34,44 @@ public class PackageComponent extends Component
 		this.base = new File(pl.getDataFolder(), "packages");
 		this.packages = new UMap<String, PackageData>();
 		
+		if(!setupPermissions())
+		{
+			pl.f("FAILED TO INSTANTIATE VAULT! ENRUE YOU HAVE INSTALLED VAULT");
+		}
+		
+		else
+		{
+			pl.s("Hooked into Vault");
+		}
+		
 		if(!base.exists())
 		{
 			pl.getDataComponent().verify(base);
 		}
+		
+		File demo = new File(base, "demo");
+		
+		if(!demo.exists())
+		{
+			demo.mkdirs();
+		}
+		
+		File demof = new File(demo, "demo-package.yml");
+		PackageData pDemo = new PackageData("Package Name", "Package Description", 20, "perm.something,perm.granted,permission.some.perm", "/give $$ diamond 1,/give $$ emerald 1,", Material.SAPLING.toString());
+		DataManager dm = new DataManager(pl, demof);
+		dm.writeYAML(pDemo);
+	}
+	
+	private boolean setupPermissions()
+	{
+		RegisteredServiceProvider<Permission> rsp = pl.getServer().getServicesManager().getRegistration(Permission.class);
+		perms = rsp.getProvider();
+		return perms != null;
 	}
 	
 	public void disable()
 	{
-		
+	
 	}
 	
 	public void load()
@@ -149,5 +185,25 @@ public class PackageComponent extends Component
 		
 		pl.gpd(p).setPackages(new UList<String>(pl.gpd(p).getPackages()).qadd(getCodeName(pd)).toString(","));
 		pl.getNoteComponent().take(p, pd.getCost());
+		p.sendMessage(Info.TAG_NOTES + "Unlocked " + pd.getName());
+		
+		for(String i : pd.getInitial().split(","))
+		{
+			String m = StringUtils.replace(i, "$$", p.getName());
+			
+			if(m.substring(0, 1).equals("/"))
+			{
+				m = m.substring(1);
+			}
+			
+			pl.s("Dispatching Command [" + p.getName() + "]: " + ChatColor.GOLD + i + " >> " + ChatColor.AQUA + m);
+			Bukkit.dispatchCommand(pl.getServer().getConsoleSender(), m);
+		}
+		
+		for(String i : pd.getGrant().split(","))
+		{
+			pl.s("Granting Permission [" + p.getName() + "]: " + ChatColor.GOLD + i);
+			
+		}
 	}
 }
